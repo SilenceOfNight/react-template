c45d5f9cc55c8628af1befe165ec25c4ea4ca873
SilenceOfNight <yiyushangzhi@163.com>
2019-04-10 22:58:02 +0800

    示例界面内容整改

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import image from '@/asserts/bg.jpg';
 import './index.css';
 
 const App = () => {
   return (
     <div className="app-container">
-      <div className="app-sider">Sider</div>
-      <div className="app-content">
-        <img src={image} alt="Losing" />
-      </div>
+      <h1>Hello React</h1>
     </div>
   );
 };
