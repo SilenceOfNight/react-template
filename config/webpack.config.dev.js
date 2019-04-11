943b46f69a72c4e8e11bf4580136df3869fa9dbd
SilenceOfNight <yiyushangzhi@163.com>
2019-04-11 23:51:50 +0800

    1. 支持端口可配置 2. 只输出错误日志

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,14 +2,18 @@ const paths = require('./paths');
 const merge = require('webpack-merge');
 const config = require('./webpack.config');
 
+const DEFAULT_SERVER_PORT = 3000;
+const port = process.env.PORT || DEFAULT_SERVER_PORT;
+
 module.exports = merge(config, {
   devServer: {
     contentBase: paths.appDist,
     compress: true,
-    port: 3000,
+    port: port,
     open: true,
     overlay: true,
     hot: true,
+    stats: 'errors-only',
     // proxy: {
     //   '/api': {
     //     target: 'http://127.0.0.1:8080',
